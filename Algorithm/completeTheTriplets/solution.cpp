4ab8e19b29f0760324c8947a8a8880205c2f5e8d
CreativeArindam <arindamlearn8@gmail.com>
2020-08-17 18:26:55 +0530

    Added completeTheTriplets(C++)

---
+++ Algorithm/completeTheTriplets/solution.cpp
@@ -0,0 +1,11 @@
+vector<int> compareTriplets(vector<int> a, vector<int> b) {
+    vector<int> result = {0,0};
+    for(int i=0; i <3 ; i++){
+        if(a.at(i) > b.at(i)){
+            result[0]++;
+        }else if(b.at(i) > a.at(i)){
+            result[1]++;
+        }
+    }
+    return result;
+}
