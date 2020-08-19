e9a1c91d23ae9bcaad31fd5a5c1503278ed927c3
Arindam Hazra <arindamlearn8@gmail.com>
2020-08-19 12:09:02 +0530

    Update solution.cpp

--- Algorithm/drawingBook/solution.cpp
+++ Algorithm/drawingBook/solution.cpp
@@ -6,7 +6,7 @@ int pageCount(int n, int p) {
         // Here is a trick. I am convering last page as page 0
         pageTurnBack = -((p-n)/2); 
     }else{
-        // Here is a trick. I am convering last page as page 0
+        // Here is a trick. I am convering last page as page 1
         pageTurnBack = -((p-n-1)/2); 
     }
     return pageTurnFront < pageTurnBack ? pageTurnFront : pageTurnBack;
